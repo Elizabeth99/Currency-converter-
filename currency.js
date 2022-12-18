a5fb577dcff1b8d4ecc79610eebd5fe9ded7c75d
Elizabeth99 <elizabethannajohn@gmail.com>
2022-12-18 11:27:17 +0530

    fourth commit

--- currency.js
+++ currency.js
@@ -1,15 +1,20 @@
 document.querySelector('#form').onsubmit = () =>{
     const base = document.querySelector('#currency-from').value;
+
+    // fetching the exchange rate api
     fetch(`https://api.exchangerate.host/latest?/source=ecb&base=${base}`)
         .then((response) => response.json())
         .then((data) => {
             const amount = document.querySelector("#amount").value;
             const currencyTo = document.querySelector("#currency-to").value;
             const rate = data.rates[currencyTo];
+            
             function convert(){
+                // converting the data
                 return amount * rate;
             }
 
+            // displaying the result
             document.querySelector(".display-result").innerHTML = `${amount} ${base.toUpperCase()} equal to ${currencyTo} ${convert().toFixed(4)}`;
         })
         .catch((error) => {
